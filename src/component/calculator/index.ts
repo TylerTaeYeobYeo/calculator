15f5a80a9ca0a5010fa0ce11c66f516af8b09043
Taeyeob Yeo <id1423fo@gmail.com>
2023-05-13 20:33:30 +0900

    fix feat: calculator logic 요구사항에 맞게 수정

--- src/component/calculator/index.ts
+++ src/component/calculator/index.ts
@@ -12,15 +12,14 @@ enum Operator {
 export class Calculator {
   private _calculatorDescription;
   private _parent;
-  private _acc: number = 0;
-  private _operator: Operator | null = null;
-  private _operand: number = 0;
+  private _accumulator: (number | Operator)[] = [];
   private _currentNumber: string | null = '0';
+  private readonly _calculatorInputRegExp = /[0-9-+*/=.]$/;
 
   private _dom: {
     calculator?: HTMLElement | null;
     keys?: HTMLElement | null;
-    display?: HTMLElement | null;
+    display?: HTMLInputElement | null;
   } = {};
 
   constructor({
@@ -31,7 +30,7 @@ export class Calculator {
     this._calculatorDescription = `
       <div class="calculator">
         <div class="calculator__display">
-          <div class="calculator__display__result"></div>
+          <input class="calculator__display__result" type="text"></input>
         </div>
         <div class="calculator__keys">
           <button class="calculator__keys__key calculator__keys__gray" data-action="clear">C</button>
@@ -59,7 +58,7 @@ export class Calculator {
 
     this.render();
 
-    this.init();
+    this._init();
   }
 
   render() {
@@ -70,13 +69,13 @@ export class Calculator {
     }
   }
 
-  init() {
-    console.log('init');
+  private _init() {
+    console.log('_init');
     if (!this._parent) return;
     this._dom.calculator = this._parent.querySelector('.calculator');
     this._dom.keys = this._dom.calculator!.querySelector('.calculator__keys');
-    this._dom.display = this._dom.calculator!.querySelector('.calculator__display');
-    this.displayNumber(this._currentNumber || this._acc.toString());
+    this._dom.display = this._dom.calculator!.querySelector('.calculator__display__result')
+    this._displayNumber();
     this._dom.keys!.addEventListener('click', (e) => {
       const target = e.target as HTMLElement;
       const action = target.getAttribute('data-action');
@@ -85,80 +84,203 @@ export class Calculator {
       if (target?.classList?.contains("calculator__keys__key")) {
         switch (action) {
           case 'decimal':
-            if (this._currentNumber === null) this._currentNumber = '0.';
-            else if (this._currentNumber?.indexOf('.') === -1) {
-              if (this._currentNumber === null) this._currentNumber = '0';
-              this._currentNumber += '.';
-            }
+            this._writeDecimal();
             break;
           case 'number':
-            if (this._currentNumber === '0' || this._currentNumber === null) {
-              this._currentNumber = target.innerHTML;
-            } else {
-              this._currentNumber += target.innerHTML;
-            }
+            this._writeNumber(target.textContent!);
             break;
           case 'clear':
-            this._acc = 0;
-            this._operator = null;
-            this._currentNumber = '0';
+            this._clear();
             break;
           case 'sign':
-            if (this._currentNumber === null) {
-              this._acc = this._acc * -1;
-            }
-            else {
-              this._currentNumber = (parseFloat(this._currentNumber) * -1).toString();
-            }
+            this._writeSign();
             break;
           case 'percentage':
-            if (this._currentNumber === null) {
-              this._acc = this._acc / 100;
-            }
-            else {
-              this._acc = (parseFloat(this._currentNumber) / 100);
-              this._currentNumber = null;
-            }
             break;
           case Operator.add:
           case Operator.subtract:
           case Operator.multiply:
           case Operator.divide:
-            if (this._currentNumber !== null) {
-              this._acc = parseFloat(this._currentNumber);
-            }
-            this._operator = action;
-            this._currentNumber = '0';
+            this._writeOperator(action);
             break;
           case 'calculate':
-            this._operand = this._currentNumber !== null ? parseFloat(this._currentNumber) : this._operand;
-            console.log(this._acc, this._operator, this._operand, this._currentNumber);
-            if (Number.isNaN(this._operand)) return;
-            if (!this._operator) return;
-            switch (this._operator) {
-              case Operator.add:
-                this._acc += this._operand;
-                break;
-              case Operator.subtract:
-                this._acc -= this._operand;
-                break;
-              case Operator.multiply:
-                this._acc *= this._operand;
-                break;
-              case Operator.divide:
-                this._acc /= this._operand;
-                break;
-            }
-            this._currentNumber = null;
+            this._calculate();
             break;
         }
       }
-      this.displayNumber(this._currentNumber || this._acc.toString());
+      this._displayNumber();
     });
+    this._dom.display?.addEventListener('input', (e) => {
+      const event = e as InputEvent;
+      const target = event.target as HTMLInputElement;
+      console.log(e);
+      let input: string | null = event.data;
+      if (input === null) {
+        this._currentNumber = null;
+        return;
+      }
+      else if (!this._calculatorInputRegExp.test(input)) {
+        this._displayNumber();
+        return;
+      }
+      if (input === '.') {
+        this._currentNumber = target.value;
+        this._removeWrongDecimal();
+        this._displayNumber();
+      }
+      else if (event.target) {
+        this._currentNumber = null;
+        this._writeNumber(target.value);
+        this._displayNumber();
+      }
+    })
+  }
+
+  private _writeDecimal() {
+    if (this._currentNumber === null) {
+      this._currentNumber = '0.';
+    }
+    else if (this._currentNumber.indexOf('.') === -1) {
+      this._currentNumber += '.';
+    }
+  }
+
+  private _removeWrongDecimal() {
+    if (this._currentNumber === null) return;
+    const index: number = this._currentNumber.indexOf('.');
+    if (index === -1) return;
+    const valueWithoutDecimal = this._currentNumber?.split('.').join('');
+    this._currentNumber = valueWithoutDecimal?.substring(0, index) + '.' + valueWithoutDecimal?.substring(index);
+  }
+
+  private _writeNumber(value: string) {
+    if (this._currentNumber === '0' ||
+      this._currentNumber === null
+    ) {
+      if (this._accumulator.length === 1) this._accumulator.pop();
+      this._currentNumber = value;
+    } else if (this._currentNumber === '-0') {
+      this._currentNumber = '-' + value;
+    } else {
+      this._currentNumber += value;
+    }
+    console.log("write number:", this._currentNumber);
+  }
+
+  private _writeOperator(operator: Operator) {
+    if (this._currentNumber === null) {
+      if (this._accumulator.length === 0) {
+        this._accumulator.push(0);
+      }
+      else if (typeof this._accumulator[this._accumulator.length - 1] !== 'number') {
+        this._accumulator.pop();
+      }
+    }
+    else this._accumulator.push(parseFloat(this._currentNumber));
+    this._accumulator.push(operator);
+    this._currentNumber = null;
+    console.log("write operator:", this._accumulator);
+  }
+
+  private _writeSign() {
+    if (this._currentNumber === null) {
+      const queued = this._getLatestQueuedNumber();
+      if (queued !== null) {
+        this._accumulator = [];
+        this._currentNumber = this._reverseSign(queued.toString());
+        return;
+      }
+      this._currentNumber = '-0';
+    } else {
+      this._currentNumber = this._reverseSign(this._currentNumber);
+    }
+  }
+
+  private _reverseSign(value: string) {
+    return value.charAt(0) === '-' ? value.slice(1) : '-' + value;
+  }
+
+  private _clear() {
+    this._accumulator = [];
+    this._currentNumber = null;
+  }
+
+  private _getLatestQueuedNumber(): number | null {
+    let int: number | null = null;
+    for (let i = this._accumulator.length - 1; i >= 0; i--) {
+      if (typeof this._accumulator[i] === 'number') {
+        int = this._accumulator[i] as number;
+        break;
+      }
+    }
+    return int;
+  }
+
+  private _displayNumber() {
+    const number = this._currentNumber ?? this._getLatestQueuedNumber()?.toString() ?? '0';
+    console.log("display number:", number);
+    if (this._dom.display) {
+      this._dom.display.value = number;
+    }
   }
   
-  displayNumber(number: string) {
-    document.querySelector('.calculator__display__result')!.innerHTML = number;
+  private _calculate() {
+    if (this._accumulator.length === 0) {
+      if (this._currentNumber !== null) {
+        this._accumulator.push(parseFloat(this._currentNumber));
+        this._currentNumber = null;
+      }
+    }
+    else {
+      if (this._currentNumber !== null) {
+        this._accumulator.push(parseFloat(this._currentNumber));
+        this._currentNumber = null;
+      }
+      else {
+        if (typeof this._accumulator[this._accumulator.length - 1] !== 'number') {
+          this._accumulator.pop();
+        }
+      }
+    }
+    this._accumulator = [this.calculate(this._accumulator)];
+    this._currentNumber = null;
+  }
+
+  private _calcTwoNumber(a: number, b: number, operator: Operator): number {
+    switch (operator) {
+      case Operator.add:
+        return a + b;
+      case Operator.subtract:
+        return a - b;
+      case Operator.multiply:
+        return a * b;
+      case Operator.divide:
+        return  a / b;
+    }
+  }
+  
+  calculate(equation: (number | Operator)[]): number {
+    if (equation.length === 0) return 0;
+    // multiplication/division
+    const eqCopy = [...equation];
+    for (let i = 0; i < eqCopy.length; i++) {
+      if (eqCopy[i] === Operator.multiply || eqCopy[i] === Operator.divide) {
+        const result = this._calcTwoNumber(eqCopy[i - 1] as number, eqCopy[i + 1] as number, eqCopy[i] as Operator);
+        if (Number.isNaN(result)) return result;
+        eqCopy.splice(i - 1, 3, result);
+        i--;
+      }
+    }
+    // add/subtract
+    for (let i = 0; i < eqCopy.length; i++) {
+      if (eqCopy[i] === Operator.add || eqCopy[i] === Operator.subtract) {
+        const result = this._calcTwoNumber(eqCopy[i - 1] as number, eqCopy[i + 1] as number, eqCopy[i] as Operator);
+        if (Number.isNaN(result)) return result;
+        eqCopy.splice(i - 1, 3, result);
+        i--;
+      }
+    }
+    return eqCopy[0] as number;
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,4 @@ import { Calculator } from './component/calculator';
 
 const app = document.querySelector<HTMLDivElement>('.app');
 
-const calculator = new Calculator({ wrapperElement: app });
+new Calculator({ wrapperElement: app });
