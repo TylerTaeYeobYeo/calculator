b72e3694e647ead761bb8b3fb7fa502d538c0a8b
Taeyeob Yeo <id1423fo@gmail.com>
2023-05-13 12:39:41 +0900

    feat: calculator logic

--- src/component/calculator/index.ts
+++ src/component/calculator/index.ts
@@ -1,13 +1,22 @@
-
 export type CalculatorProps = {
   wrapperElement: HTMLElement | null;
 };
 
+enum Operator {
+  add = 'add',
+  subtract = 'subtract',
+  multiply = 'multiply',
+  divide = 'divide',
+}
+
 export class Calculator {
   private _calculatorDescription;
   private _parent;
-  private _acc = '0';
-  private _queue: (string | number)[] = [];
+  private _acc: number = 0;
+  private _operator: Operator | null = null;
+  private _operand: number = 0;
+  private _currentNumber: string | null = '0';
+
   private _dom: {
     calculator?: HTMLElement | null;
     keys?: HTMLElement | null;
@@ -67,6 +76,7 @@ export class Calculator {
     this._dom.calculator = this._parent.querySelector('.calculator');
     this._dom.keys = this._dom.calculator!.querySelector('.calculator__keys');
     this._dom.display = this._dom.calculator!.querySelector('.calculator__display');
+    this.displayNumber(this._currentNumber || this._acc.toString());
     this._dom.keys!.addEventListener('click', (e) => {
       const target = e.target as HTMLElement;
       const action = target.getAttribute('data-action');
@@ -74,38 +84,81 @@ export class Calculator {
       console.log(action);
       if (target?.classList?.contains("calculator__keys__key")) {
         switch (action) {
-          case 'clear':
-            this._queue = [];
-            break;
-          case 'sign':
-            break;
           case 'decimal':
+            if (this._currentNumber === null) this._currentNumber = '0.';
+            else if (this._currentNumber?.indexOf('.') === -1) {
+              if (this._currentNumber === null) this._currentNumber = '0';
+              this._currentNumber += '.';
+            }
             break;
           case 'number':
+            if (this._currentNumber === '0' || this._currentNumber === null) {
+              this._currentNumber = target.innerHTML;
+            } else {
+              this._currentNumber += target.innerHTML;
+            }
             break;
-          case 'add':
+          case 'clear':
+            this._acc = 0;
+            this._operator = null;
+            this._currentNumber = '0';
             break;
-          case 'subtract':
+          case 'sign':
+            if (this._currentNumber === null) {
+              this._acc = this._acc * -1;
+            }
+            else {
+              this._currentNumber = (parseFloat(this._currentNumber) * -1).toString();
+            }
             break;
-          case 'multiply':
+          case 'percentage':
+            if (this._currentNumber === null) {
+              this._acc = this._acc / 100;
+            }
+            else {
+              this._acc = (parseFloat(this._currentNumber) / 100);
+              this._currentNumber = null;
+            }
             break;
-          case 'divide':
+          case Operator.add:
+          case Operator.subtract:
+          case Operator.multiply:
+          case Operator.divide:
+            if (this._currentNumber !== null) {
+              this._acc = parseFloat(this._currentNumber);
+            }
+            this._operator = action;
+            this._currentNumber = '0';
             break;
           case 'calculate':
+            this._operand = this._currentNumber !== null ? parseFloat(this._currentNumber) : this._operand;
+            console.log(this._acc, this._operator, this._operand, this._currentNumber);
+            if (Number.isNaN(this._operand)) return;
+            if (!this._operator) return;
+            switch (this._operator) {
+              case Operator.add:
+                this._acc += this._operand;
+                break;
+              case Operator.subtract:
+                this._acc -= this._operand;
+                break;
+              case Operator.multiply:
+                this._acc *= this._operand;
+                break;
+              case Operator.divide:
+                this._acc /= this._operand;
+                break;
+            }
+            this._currentNumber = null;
             break;
         }
       }
-      document.querySelector('.calculator__display__result')!.innerHTML = this.getLatestNumber(this._queue);
+      this.displayNumber(this._currentNumber || this._acc.toString());
     });
   }
   
-  getLatestNumber(queue: (string| number)[]): string {
-    for (let i = queue.length - 1; i >= 0; i--) {
-      if (!Number.isNaN(Number(queue[i]))) {
-        return queue[i].toString();
-      }
-    }
-    return '0';
+  displayNumber(number: string) {
+    document.querySelector('.calculator__display__result')!.innerHTML = number;
   }
 }
 
